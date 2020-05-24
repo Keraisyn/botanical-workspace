47e7af8592b8206f42916215d99d85721e4d76cd
Cyndakwil <rshetty449@gmail.com>
2020-05-24 01:21:43 -0400

    fixed update post

--- app.py
+++ app.py
@@ -18,21 +18,13 @@ def kill():
         return json.jsonify({"data": "kill"})
         #kill here
 
-@app.route('/water', methods=["POST"])
-@cross_origin(origin='*',headers=['Content-Type'])
-def water():
-    if request.method == "POST":
-        print("water")
-        return json.jsonify({"data": "water"})
-        #water here
-
 @app.route('/update', methods=["POST"]) 
 @cross_origin(origin='*',headers=['Content-Type'])
 def update():
     if request.method == "POST":
         ser.write(b"r")
         data = ser.readline()
-        return json.jsonify({"data": "update received"})
+        return json.jsonify(request.json)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
